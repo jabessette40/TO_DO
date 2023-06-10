80d67440cee2b94970a6ef38b4c196d91d193a13
Jeff Bessette <jabessette25@gmail.com>
2023-06-10 14:04:52 -0400

    Worked in app.py

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 # Simple ToDo App made in Python.
-
+def add_task(task_list, task):
+    '''Add TASK to TASK_LIST'''
+    return task_list.append(task)
+    
 def main():
-    print("Testing")
+    
+    todo_list = []
+    
+    add_task(todo_list, "Finish Project")
+    
+    print(todo_list)
 
 
 if __name__ == "__main__":
