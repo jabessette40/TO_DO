140489af1486be6c6d48ed407fa54b13a06962c4
Jeff Bessette <jabessette25@gmail.com>
2023-06-10 19:09:02 -0400

    Worked in app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 # Simple ToDo App made in Python.
+
 def add_task(task_list, task):
     '''Add TASK to TASK_LIST'''
     return task_list.append(task)
@@ -17,9 +18,7 @@ def main():
         print("ToDo List: V)iew List. A)dd Task.  D)elete Task.  E)xit.\n")
         event = input("What would you like to do? ")
 
-        if event == "E" or "e":
-            continue_flag = False
-        elif event == "A" or "a":
+        if event == "A" or "a":
             task = input("Enter task: ")
             add_task(todo_list, task)
         elif event == "V" or "v":
@@ -27,6 +26,7 @@ def main():
                 view_list(todo_list)
             else:
                 print("Todo list is empty! Add a task!")
-
+        elif event == "E" or "e":
+            continue_flag = False
 if __name__ == "__main__":
     main()
