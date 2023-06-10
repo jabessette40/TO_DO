e13afd57426af54f38f23a7f3ac926370f00c221
Jeff Bessette <jabessette25@gmail.com>
2023-06-10 14:42:29 -0400

    Worked in app.py

--- app.py
+++ app.py
@@ -2,14 +2,18 @@
 def add_task(task_list, task):
     '''Add TASK to TASK_LIST'''
     return task_list.append(task)
-    
+
+
 def main():
-    
-    todo_list = []
-    
-    add_task(todo_list, "Finish Project")
-    
-    print(todo_list)
+
+    continue_flag = True
+
+    while continue_flag:  # Event loop
+        print("ToDo List: A)dd Task.  D)elete Task.  E)xit.\n")
+
+        event = input("What would you like to do? ")
+        if event == "E":
+            continue_flag = False
 
 
 if __name__ == "__main__":
