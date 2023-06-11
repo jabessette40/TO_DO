64c74d48eda0f509813b6ea0734a0dc9f5515fdd
Jeff Bessette <jabessette25@gmail.com>
2023-06-11 18:21:36 -0400

    Tested comestic changes

--- app.py
+++ app.py
@@ -1,16 +1,23 @@
 # Simple ToDo App made in Python.
 
+# To Do - 
+# Prevent delete function from running if the list is empty
+# If there is a space in the input, ie "A_" input is neglected. Strip whitespace.
+# Add checklist toggle
+# Add save/load feature.
 
 def add_task(task_list, task):
     '''Add TASK to TASK_LIST'''
     return task_list.append(task)
 
 
-def view_list(task_list):
+def view_list(task_list, border_char):
     '''Print TASK_LIST to stdout'''
     index = 0
     for task in task_list:
-        print(str(index) + " " + task)
+        print(border_char * 25)
+        print("| " + str(index) + " | " + task)
+
         index += 1
 
 
@@ -23,8 +30,12 @@ def del_task(task_list):
     choice = input("Delete which task? ")
 
     return int(choice)
-def draw_gui():
-    print("ToDo List: V)iew List. A)dd Task.  D)elete Task.  E)xit.\n")
+def draw_gui(border_char):
+    ''' Draw the menu'''
+    print() # For newline
+    print("ToDo List: V)iew List. A)dd Task.  D)elete Task.  E)xit.")
+    print(border_char * 55)
+    print() # For newline
 
 def main():
 
@@ -32,7 +43,7 @@ def main():
     todo_list = []
 
     while continue_flag:  # Event loop
-        draw_gui()
+        draw_gui("-")
         event = input("What would you like to do? ")
 
         if event in ("A", "a"):
@@ -40,7 +51,7 @@ def main():
             add_task(todo_list, task)
         elif event in ("V", "v"):
             if todo_list:
-                view_list(todo_list)
+                view_list(todo_list, "-")
             else:
                 print("Todo list is empty! Add a task!")
         elif event in ("D", "d"):
