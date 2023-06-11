e87c381220c2d5e58cc7da5fa51585f356bbd884
Jeff Bessette <jabessette25@gmail.com>
2023-06-10 21:04:04 -0400

    Worked in app.py.

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 # Simple ToDo App made in Python.
 
-#To Do FINISH del_task function.
+# To Do - Prevent Index Out Of Range error while deleting an item. 
 
 def add_task(task_list, task):
     '''Add TASK to TASK_LIST'''
     return task_list.append(task)
+
 
 def view_list(task_list):
     '''Print TASK_LIST to stdout'''
@@ -13,12 +14,16 @@ def view_list(task_list):
         print(str(index) + " " + task)
         index += 1
 
+
 def del_task(task_list):
     '''Delete a task from TASK_LIST'''
     index = 0
     for task in task_list:
         print(str(index) + " " + task)
         index += 1
+    choice = input("Delete which task? ")
+
+    return int(choice)
 
 
 def main():
@@ -35,12 +40,14 @@ def main():
             add_task(todo_list, task)
         elif event in ("V", "v"):
             if todo_list:
-                view_list(todo_list )
+                view_list(todo_list)
             else:
                 print("Todo list is empty! Add a task!")
         elif event in ("D", "d"):
-            del_task(todo_list)
+            del todo_list[del_task(todo_list)] # I needed to make this clearer. 
         elif event in ("E", "e"):
             continue_flag = False
+
+
 if __name__ == "__main__":
     main()
