aa2a67ccc590e78f53f1b35093ece266f4d96613
Jeff Bessette <jabessette25@gmail.com>
2023-06-18 10:39:54 -0400

    Restructured Project

---
+++ src/to_do_jabessette40/to_do_mod.py
@@ -0,0 +1,32 @@
+def add_task(task_list, task):
+    '''Add TASK to TASK_LIST'''
+    return task_list.append(task)
+
+
+def view_list(task_list):
+    '''Print TASK_LIST to stdout'''
+    index = 0
+    for task in task_list:
+        print(str(index) + " " + task)
+
+        index += 1
+
+
+def del_task(task_list):
+    '''Delete a task from TASK_LIST'''
+    index = 0
+    for task in task_list:
+        print(str(index) + " " + task)
+        index += 1
+    choice = input("Delete which task? ")
+
+    return int(choice)
+
+
+def draw_gui(border_char):
+    ''' Draw the menu'''
+    print()  # For newline
+    print("ToDo List: V)iew List. A)dd Task.  D)elete Task.  E)xit.")
+    print(border_char * 56)
+    print()  # For newline
+
